e0b9a6141e716747d9a440a033cf75f635cc9916
Mixbo <mixbo@wakowakowako.com>
2014-02-12 13:38:44 -0500

    Started Controller and Gameboard definitions

--- Controller.py
+++ Controller.py
@@ -1,15 +1,28 @@
+from Gameboard import Gameboard
+from GameObject import *
+
 class Controller(object):
 	"""Makes the path between the model and the view"""
 
 	def __init__(self):
 		self.reset()
 
-	def initGameboard(self):
+	def initGameboard(self,x,y):
+		self.gameboard = Gameboard(x,y)
+
 	def initDalekArmy(self):
+		self.daleks = []
+		for i in range(self.doctor.level*5)
+			self.daleks.append(Dalek())
+
 	def initDoctor(self):
+		self.doctor = Doctor()
+
 	def isDoctorSafe(self):
 	def canDoctorMove(self):
 	def moveDoctor(self):
 	def moveDaleks(self):
 	def reset(self):
-		
+		initGameboard(20,30)
+		initDoctor()
+		initDalekArmy()

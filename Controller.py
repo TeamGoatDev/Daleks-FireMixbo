f9ce9af39970e2c69c48fc1cdf4651ea8f5344b4
Mixbo <mixbo@wakowakowako.com>
2014-02-12 13:10:57 -0500

    Controller method def

---
+++ Controller.py
@@ -0,0 +1,15 @@
+class Controller(object):
+	"""Makes the path between the model and the view"""
+
+	def __init__(self):
+		self.reset()
+
+	def initGameboard(self):
+	def initDalekArmy(self):
+	def initDoctor(self):
+	def isDoctorSafe(self):
+	def canDoctorMove(self):
+	def moveDoctor(self):
+	def moveDaleks(self):
+	def reset(self):
+		

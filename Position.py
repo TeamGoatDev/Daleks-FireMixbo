ab70c7c4369fa38ff60e0e48bc062691d712c43f
fireraccoon <willyjames.music@gmail.com>
2014-02-16 16:42:50 -0500

    Create Position.py

---
+++ Position.py
@@ -0,0 +1,14 @@
+from random import randrange
+
+class Position(object):
+	"""docstring for Position"""
+	def __init__(self, x,y):
+		super(Position, self).__init__()
+		self.x = x
+		self.y = y
+
+
+	def randomize(self, rangeX, rangeY):
+		""" rangeX and rangeY are Tuples(min,max)"""
+		self.x = randrange(rangeX[0], rangeX[1]+1)
+		self.y = randrange(rangeY[0], rangeY[1]+1)

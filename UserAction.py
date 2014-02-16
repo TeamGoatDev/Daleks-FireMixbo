0ab8d769dc4a4a6349764e4035cb1d8764519a74
fireraccoon <willyjames.music@gmail.com>
2014-02-16 16:47:09 -0500

    Create UserAction.py

---
+++ UserAction.py
@@ -0,0 +1,27 @@
+     
+class Enum(object):
+	"""docstring for Enum"""
+	def __init__(self):
+		super(Enum, self).__init__()
+		self.lastId = 0
+	def addId(self):
+		self.lastId +=1
+		return self.lastId
+		
+
+
+class UserAction:
+        """docstring for UserAction"""
+        Enum = Enum()
+        MOVE_N = Enum.addId()
+        MOVE_S = Enum.addId()
+        MOVE_E = Enum.addId()
+        MOVE_W = Enum.addId()
+
+        MOVE_NW = Enum.addId()
+        MOVE_NE = Enum.addId()
+        MOVE_SW = Enum.addId()
+        MOVE_SE = Enum.addId()
+
+        ZAP = Enum.addId()
+        TELEPORT = Enum.addId()

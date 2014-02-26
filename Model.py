05ca0146642d0b5e60a80be0c43942cabb39c7f3
Mixbo <mixbo@wakowakowako.com>
2014-02-26 09:03:28 -0500

    Maybe fixed the game board size (freaking off by 1)

--- Model.py
+++ Model.py
@@ -124,9 +124,9 @@ class Model(object):
 
 
     def canDoctorMove(self, position):
-        if position.x < 1 or position.x > self.gameboard.x:
+        if position.x < 0 or position.x > self.gameboard.x-1:
             return False
-        if position.y < 1 or position.y > self.gameboard.y:
+        if position.y < 0 or position.y > self.gameboard.y-1:
             return False
         if self.isDoctorDead(position): #Test whether or not the doctor is commiting suicide
             return False
